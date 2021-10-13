e2dd605b3d6a489831e95d9adb1bd30559b2c1ef
Ivana Vucenovic <ivucenovic@hotmail.com>
2021-10-13 17:53:37 -0400

    third commit

--- books_authors_app/models.py
+++ books_authors_app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    decs = models.TextField()
+    description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Author(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
-    notes = models.TextField()
+    notes = models.CharField(max_length=255)
     books = models.ManyToManyField(Book, related_name='authors')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
