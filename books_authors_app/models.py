f0ccbb3b942235b7b47acde7463cdcab64cb150e
Ivana Vucenovic <ivucenovic@hotmail.com>
2021-10-12 23:27:11 -0400

    second commit

--- books_authors_app/models.py
+++ books_authors_app/models.py
@@ -14,7 +14,9 @@ class Author(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 
